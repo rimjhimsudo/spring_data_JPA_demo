a3d8e5b7d38b2a38d9e646b104b873d52abc3a1c
rimjhimsudo <rimjhimvaish@gmail.com>
2022-01-13 20:13:53 +0530

    databse connection seup, app up and running

---
+++ src/test/java/com/springwithmysqldemo/sqlspringdemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwithmysqldemo.sqlspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
