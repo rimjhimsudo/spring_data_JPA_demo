952a5a4b9e89b251f25f140bc9ee5a57b66ebb22
rimjhimsudo <rimjhimvaish@gmail.com>
2022-01-28 22:32:37 +0530

    employee post api using thread pool executor successfully

---
+++ src/main/java/com/springwithmysqldemo/sqlspringdemo/Task.java
@@ -0,0 +1,13 @@
+package com.springwithmysqldemo.sqlspringdemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.concurrent.Callable;
+
+public class Task implements Callable {
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+}
--- src/main/java/com/springwithmysqldemo/sqlspringdemo/service/EmployeeService.java
+++ src/main/java/com/springwithmysqldemo/sqlspringdemo/service/EmployeeService.java
@@ -6,15 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
     public void save(List<Employee> employeeList){
-        employeeRepository.saveAll(employeeList);
+        Future<String> future=null;
+        ExecutorService executorService= Executors.newFixedThreadPool(10);
+        Callable<String> callableTask = () -> {
+            employeeRepository.saveAll(employeeList);
+            return "Task's execution";
+        };
+        for(int i=0;i<=100;i++){
+            future=executorService.submit(callableTask);
+        }
+        String result = null;
+        try {
+            result = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread name : "+ Thread.currentThread().getName());
+        executorService.shutdown();
     }
+
+
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
     }
 }
+
